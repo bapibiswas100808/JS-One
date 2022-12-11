06a9f75b289c9cf3eb46ba2a9403b451c4e08ffd
Bapi Biswas <star.bappy@gmail.com>
2022-12-11 13:41:44 +0600

    homework completed

---
+++ script.js
@@ -0,0 +1,20 @@
+//let life = "beautiful";
+//if (life === "beautiful"); alert("Life is great");
+
+//console.log(5 + 6 + 8);
+
+// let firstName = "rahim";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+console.log(10000000);
+
+console.log("I Love Bangladesh");
+
+let firstName = "asif";
+console.log(firstName);
